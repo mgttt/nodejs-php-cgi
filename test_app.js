a3bc79bfedf98fbac326143c9d75306aca329965
WANJO CHAN <wanjochan@users.noreply.github.com>
2017-03-26 11:42:36 +0800

    Update test_app.js

--- test_app.js
+++ test_app.js
@@ -5,18 +5,14 @@ function argv2o(argv){
 	for(k in argv){
 		var m,mm,v=argv[k];
 		argv_o[""+k]=v;
-		if (m=v.match(/^--?([a-zA-Z0-9-_]*)=(.*)/)){
-			if(mm=m[2].match(/^".*"$/)){
-				argv_o[m[1]]=mm[1];
-			}else{
-				argv_o[m[1]]=m[2];
-			}
-		}
+		(m=v.match(/^--?([a-zA-Z0-9-_]*)=(.*)/))&&(argv_o[m[1]]=(mm=m[2].match(/^".*"$/))?mm[1]:m[2]);
 	}
 	return argv_o;
 }
+
 var argo=argv2o(process.argv);//console.log(argo)
 console.log(argo);
+
 var http_server=require('http').createServer(require("./php-cgi.js")({ bin:argo.cgi }))
 .listen(argo.port||80,argo.host||'0.0.0.0',()=>{
 	console.log(argo.host||'*'+':'+argo.port);
