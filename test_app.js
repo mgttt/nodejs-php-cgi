cc8336bd9c8157f616bf1acce936410bb79ed084
wanjochan <wanjochan@gmail.com>
2017-03-26 12:05:19 +0800

    WJC: improve

--- test_app.js
+++ test_app.js
@@ -1,23 +1,17 @@
 //eg:
-//node test_app.js -port=1338 -cgi=`which php-cgi`
+//node test_app.js [-port=1338] -cgi=`which php-cgi`
 function argv2o(argv){
 	var argv_o={};
 	for(k in argv){
 		var m,mm,v=argv[k];
 		argv_o[""+k]=v;
-		if (m=v.match(/^--?([a-zA-Z0-9-_]*)=(.*)/)){
-			if(mm=m[2].match(/^".*"$/)){
-				argv_o[m[1]]=mm[1];
-			}else{
-				argv_o[m[1]]=m[2];
-			}
-		}
+		(m=v.match(/^--?([a-zA-Z0-9-_]*)=(.*)/))&&(argv_o[m[1]]=(mm=m[2].match(/^".*"$/))?mm[1]:m[2]);
 	}
 	return argv_o;
 }
 var argo=argv2o(process.argv);//console.log(argo)
 console.log(argo);
 var http_server=require('http').createServer(require("./php-cgi.js")({ bin:argo.cgi }))
-.listen(argo.port||80,argo.host||'0.0.0.0',()=>{
-	console.log(argo.host||'*'+':'+argo.port);
+.listen(ppp=argo.port||80,hhh=argo.host||'0.0.0.0',()=>{
+	console.log(hhh+':'+ppp);
 });
